948d387161e76b6264543e6c515a775a24ba788e
Administrator <Administrator@pgos>
2017-03-29 16:38:49 +0800

    add

---
+++ micro-share/src/main/java/com/newlivat/share/ShareApplication.java
@@ -0,0 +1,40 @@
+package com.newlivat.share;
+
+import java.util.Properties;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.newlivat.common.util.ConfigUtil;
+import com.newlivat.share.verticle.ShareVerticle;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.zookeeper.ZookeeperClusterManager;
+
+public class ShareApplication {
+	private static Properties properties = ConfigUtil.getDefaultConfig();
+	private static ClassPathXmlApplicationContext applicationContext = null;
+
+	public static void main(String[] args) {
+		// applicationContext = new
+		// ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		// ShareVerticle share =
+		// applicationContext.getBean(ShareVerticle.class);
+
+		ClusterManager mgr = new ZookeeperClusterManager();
+		VertxOptions options = new VertxOptions().setClusterManager(mgr);
+		options.setClusterHost(properties.getProperty("cluster.host", "localhost"));
+		options.setClusterPublicHost(properties.getProperty("cluster.public.host", "localhost"));
+		DeploymentOptions option = new DeploymentOptions();
+		option.setWorker(true);
+		Vertx.clusteredVertx(options, resultHandler -> {
+			if (resultHandler.succeeded()) {
+				Vertx vertx = resultHandler.result();
+				vertx.deployVerticle(ShareVerticle.class.getName(), option);
+			}
+		});
+	}
+
+}
